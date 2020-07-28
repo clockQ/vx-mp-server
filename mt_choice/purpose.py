e455f60a769369880b308afa33fbd02da831beaa
clock <2567729201@qq.com>
2020-07-28 17:48:47 +0800

    专栏优先级下移

--- mt_choice/purpose.py
+++ mt_choice/purpose.py
@@ -7,12 +7,12 @@ zhihu.load_db()
 
 all_purpose = {
     '刷新数据库': (zhihu.flush_db, [Role.SUADMIN]),
-    '随机专栏': (zhihu.random_columns, [Role.SUADMIN]),
     '随机文章': (zhihu.random_articles, [Role.FANS]),
     '给我一篇': (zhihu.specify_articles, [Role.FANS]),
     '随机出题': (zhihu.set_question, [Role.FANS]),
     '给我一道': (zhihu.set_question, [Role.FANS]),
     '给我答案': (zhihu.get_answer, [Role.FANS]),
+    '随机专栏': (zhihu.random_columns, [Role.SUADMIN]),
 }
 
 
